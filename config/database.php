ab40ff73c7a78ab54b23ec02626f612ef6b5d117
loadpwnz11-oss <loadpwnz11@gmail.com>
2026-08-19 08:31:27 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -237,12 +237,14 @@ function jsonResponse($data, $statusCode = 200) {
  * Get all headers from request
  * @return array
  */
-function getallheaders() {
-    $headers = [];
-    foreach ($_SERVER as $name => $value) {
-        if (substr($name, 0, 5) == 'HTTP_') {
-            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
         }
+        return $headers;
     }
-    return $headers;
 }
